6ed46ceda7dc80580feeb2fe5b61d62e07165b49
ronaldcc25 <racalderon13@utpl.edu.ec>
2024-05-02 16:03:14 -0500

    Uso toString

---
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete3/Hospital.java
@@ -0,0 +1,68 @@
+package paquete3;
+
+public class Hospital {
+
+    private String nombre;
+    private String ciudad;
+    private int numeroDoctores;
+    private int numeroEnfermeros;
+
+    public Hospital() {
+        nombre = "na";
+        ciudad = "ciu";
+        numeroDoctores = 100;
+        numeroEnfermeros = 400;
+    }
+
+    public Hospital(String n, String ciu, int doctores, int enfermeros) {
+
+        nombre = n;
+        ciudad = ciu;
+        numeroDoctores = doctores;
+        numeroEnfermeros = enfermeros;
+    }
+
+    public void establecerNombre(String n) {
+        nombre = n;
+    }
+
+    public void establecerCiudad(String n) {
+        ciudad = n;
+    }
+
+    public void establecerNumeroDoctores(int n) {
+        numeroDoctores = n;
+    }
+
+    public void establecerNumeroEnfermeros(int n) {
+        numeroEnfermeros = n;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public String obtenerCiudad() {
+        return ciudad;
+    }
+
+    public int obtenerNumeroDoctores() {
+        return numeroDoctores;
+    }
+
+    public int obtenerNumeroEnfermeros() {
+        return numeroEnfermeros;
+    }
+
+    @Override
+    public String toString() {
+
+        String cadena = String.format("Nombre: %s\nCiudad: %s\nNumero de "
+                + "Doctores: %d\nNumero de enfermeros: %d\n"
+                + "======================================================"
+                + "", obtenerNombre(),ciudad,obtenerNumeroDoctores(),
+                obtenerNumeroEnfermeros());
+        return cadena;
+    }
+
+}
